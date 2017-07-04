fb8b7cec6425b7d42359f8a092bf50f54f7e1182
neelkml <neelkamalsingh145@gmail.com>
2017-07-04 16:01:14 +0530

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Pyramid{
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter the number of star = ");
+int n=sc.nextInt();
+for(int i=1; i <=n; i=i+2)
+{
+for(int j=n-i; j >1; j=j-2)
+{
+System.out.print(" ");
+}
+for (int k = 1; k <=i; k++) {
+System.out.print("*"); 
+}
+System.out.println(""); 
+}
+}
+}
