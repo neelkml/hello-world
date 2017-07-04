219e9ea27aca0c0a2fd7ed6dc709ba8e9bc58805
neelkml <neelkamalsingh145@gmail.com>
2017-07-04 15:45:07 +0530

    Add files via upload

---
+++ Staatic.java
@@ -0,0 +1,16 @@
+class Staatic
+{
+static int count=0;
+public static void main(String args[])
+{
+Static obj=new Static();
+System.out.println(count);
+Static obj1=new Static();
+count++;
+System.out.println(count);
+count++;
+Static obj2= new Static();
+System.out.println(count);
+}
+}
+
